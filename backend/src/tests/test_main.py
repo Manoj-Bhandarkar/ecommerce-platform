0db8ab5d22bf03f8416c4bc3d047ebc7c58d1f5e
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-06-09 21:12:43 +0530

    feat(tests): PyTest setup and test_main done

---
+++ backend/src/tests/test_main.py
@@ -0,0 +1,16 @@
+from httpx import AsyncClient, ASGITransport
+from src.main import app
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_root_endpoint():
+
+    transport = ASGITransport(app=app)
+
+    async with AsyncClient(transport=transport, base_url="http://test") as client:
+
+        response = await client.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == {"message": "Welcome to the E-Commerce API"}
