cffe509fd67028659593987d787c96c9d9f35c98
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-06-10 09:59:24 +0530

    feat(test_auth): test_auth test_login_invalid_credentials, test_login_success, test_refresh_token_missing, test_refresh_token_invalid, test_refresh_token_success, test_send_password_reset_email

--- backend/src/tests/conftest.py
+++ backend/src/tests/conftest.py
@@ -1,6 +1,16 @@
 import sys
 from pathlib import Path
+import pytest
+from httpx import AsyncClient, ASGITransport
+from src.main import app
 
-ROOT_DIR = Path(__file__).resolve().parent
-
+ROOT_DIR = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(ROOT_DIR))
+
+
+@pytest.fixture
+async def client():
+    transport = ASGITransport(app=app)
+
+    async with AsyncClient(transport=transport, base_url="http://test") as client:
+        yield client
--- backend/src/tests/test_auth.py
+++ backend/src/tests/test_auth.py
@@ -1,27 +1,65 @@
 import pytest
-from httpx import AsyncClient, ASGITransport
-from src.main import app
 
 
 @pytest.mark.asyncio
-async def test_login_invalid_credentials():
-    transport = ASGITransport(app=app)
-    async with AsyncClient(transport=transport, base_url="http://test") as client:
-        response = await client.post(
-            "/api/v1/auth/login",
-            json={"email": "wrong@test.com", "password": "wrongpassword"},
-        )
-    assert response.status_code in [400, 401]
+async def test_login_invalid_credentials(client):
+    response = await client.post(
+        "/api/v1/auth/login",
+        json={"email": "wrong@test.com", "password": "wrongpassword"},
+    )
+    print(response.json())
+    assert response.status_code == 401
+    assert response.json()["detail"] == "Invalid credentials"
 
 
 @pytest.mark.asyncio
-async def test_login_success():
-    transport = ASGITransport(app=app)
-    async with AsyncClient(transport=transport, base_url="http://test") as client:
-        response = await client.post(
-            "/api/v1/auth/login",
-            json={"email": "manoj@gmail.com", "password": "Manoj@5424"},
-        )
+async def test_login_success(client):
+    response = await client.post(
+        "/api/v1/auth/login",
+        json={"email": "manoj@gmail.com", "password": "Manoj@5424"},
+    )
     assert response.status_code == 200
+    assert response.json()["message"] == "Login successful"
     assert "access_token" in response.cookies
     assert "refresh_token" in response.cookies
+
+
+@pytest.mark.asyncio
+async def test_refresh_token_missing(client):
+    response = await client.post("/api/v1/auth/refresh")
+    assert response.status_code == 401
+    assert response.json()["detail"] == "Missing refresh token"
+
+
+@pytest.mark.asyncio
+async def test_refresh_token_invalid(client):
+    client.cookies.set("refresh_token", "Invalide_token")
+    response = await client.post("/api/v1/auth/refresh")
+    assert response.status_code == 401
+    assert response.json()["detail"] == "Invalid refresh token"
+
+
+@pytest.mark.asyncio
+async def test_refresh_token_success(client):
+    login_response = await client.post(
+        "/api/v1/auth/login",
+        json={"email": "manoj@gmail.com", "password": "Manoj@5424"},
+    )
+    print(login_response.cookies)
+    refresh_token = login_response.cookies.get("refresh_token")
+
+    client.cookies.set("refresh_token", refresh_token)
+    assert login_response.status_code == 200
+    response = await client.post("/api/v1/auth/refresh")
+    print(response.json())
+    assert response.status_code == 200
+    assert response.json()["message"] == "Token refresh successful"
+
+
+@pytest.mark.asyncio
+async def test_send_password_reset_email(client):
+    response = await client.post(
+        "/api/v1/auth/send-password-reset-email", json={"email": "manoj@gmail.com"}
+    )
+    assert response.status_code == 200
+    assert response.json()["message"] == "Password reset link sent"
