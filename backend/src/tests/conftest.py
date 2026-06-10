ebbec105f4f097252c0a41c62def36b1ae20c4b1
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-06-10 15:36:33 +0530

    feat(test_cart): cart testing done

--- backend/src/tests/conftest.py
+++ backend/src/tests/conftest.py
@@ -11,6 +11,16 @@ sys.path.insert(0, str(ROOT_DIR))
 @pytest.fixture
 async def client():
     transport = ASGITransport(app=app)
-
     async with AsyncClient(transport=transport, base_url="http://test") as client:
         yield client
+
+
+@pytest.fixture
+async def auth_client(client):
+    login_response = await client.post(
+        "/api/v1/auth/login",
+        json={"email": "manoj@gmail.com", "password": "Manoj@5424"},
+    )
+    access_token = login_response.cookies.get("access_token")
+    client.cookies.set("access_token", access_token)
+    return client
---
+++ backend/src/tests/test_cart.py
@@ -0,0 +1,58 @@
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_get_cart(auth_client):
+    print(auth_client.cookies)
+
+    response = await auth_client.get("/api/v1/cart/")
+    print(response.json())
+    assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_add_to_cart(auth_client):
+    response = await auth_client.post(
+        "/api/v1/cart/add", json={"product_id": 722, "quantity": 1}
+    )
+    print(response.status_code)
+    print(response.json())
+    assert response.status_code in [200, 201]
+
+
+@pytest.mark.asyncio
+async def test_increase_cart_item(auth_client):
+    await auth_client.post("/api/v1/cart/add", json={"product_id": 722, "quantity": 1})
+    response = await auth_client.patch("/api/v1/cart/increase/722")
+    assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_decrease_cart_item(auth_client):
+    await auth_client.post("/api/v1/cart/add", json={"product_id": 722, "quantity": 1})
+    response = await auth_client.patch("/api/v1/cart/decrease/722")
+    assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_add_to_cart_invalid_product(auth_client):
+    response = await auth_client.post(
+        "/api/v1/cart/add", json={"product_id": 99999, "quantity": 1}
+    )
+    assert response.status_code == 404
+
+
+@pytest.mark.asyncio
+async def test_empty_cart(auth_client):
+    response = await auth_client.get("/api/v1/cart/")
+    assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_delete_cart_item(auth_client):
+    add_response = await auth_client.post(
+        "/api/v1/cart/add", json={"product_id": 722, "quantity": 1}
+    )
+    item_id = add_response.json()["id"]
+    response = await auth_client.delete(f"/api/v1/cart/delete/{item_id}")
+    assert response.status_code == 204
